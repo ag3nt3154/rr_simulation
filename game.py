81b09f1577d0f67a8d45b8c14ab3d9a18ca3df85
Han Yuan <ag3nt3154@gmail.com>
2021-05-17 13:56:56 +0800

    'test'

--- game.py
+++ game.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Gameplay bot class
 class rrbot:
 
     def __init__(self):
+        # Magazine distribution for the round
         self.mag = None
-        pass
+
     
 
     def game_start(self, p1, p2, shuffle=False, start_player=1, print1=False):
 
-        # Generate magazine
-        
+        # Generate magazine from the magazines from each player.
         self.mag = [p1.mag, p2.mag]
         if print1: print(self.mag)
 
@@ -41,6 +42,7 @@ class rrbot:
 
                     # Player 1 decision
                     play = p1.play(i, p1_poison_left, p2_poison_left, pass_mult)
+                    # If player 1 shoots
                     if play == 'shoot':
                         if print1: print('player 1 shoots')
                         p1.points += bullet_point * pass_mult
@@ -48,6 +50,7 @@ class rrbot:
                         p2_poison_left -= self.mag[1][i]
                         if print1: print(f'bullets: {self.mag[0][i]}, {self.mag[1][i]}')
                         if print1: print(f'p1: {p1.points}, p2: {p2.points}')
+                    # If player 1 passes
                     else:
                         if print1: print('player 1 passes')
                         pass_mult *= 2
@@ -180,23 +183,26 @@ class rrbot:
                             if print1: print(f'bullets: {self.mag[0][i]}, {self.mag[1][i]}')
                             if print1: print(f'p1: {p1.points}, p2: {p2.points}')
 
-                    
-
     
         if print1: print(f'player 1 points:{p1.points}')
         if print1: print(f'player 2 points:{p2.points}')
         if print1: print('******************************************')
+        # Return points
         return p1.points, p2.points
 
 
                         
-
+# Player class
 class player:
 
     def __init__(self, mag_size=8, poison_bullets=3, pass_prob=0, strat=0):
         self.points = 0
         # Generate magazine distribution
-        self.mag = [1, 1, 1, 0, 0, 0, 0, 0]
+        self.mag = []
+        for i in range(poison_bullets):
+            self.mag.append(1)
+        for j in range(mag_size - poison_bullets):
+            self.mag.append(0)
         np.random.shuffle(self.mag)
         self.pass_prob = pass_prob
         self.strat = strat
@@ -206,13 +212,18 @@ class player:
         other_poison_prob = other_poison_left / (len(self.mag) - bullet)
         self_poison_prob = self_poison_left / (len(self.mag) - bullet)
         
+        # Strat 0: pass if other player passes, shoot otherwise, small chance of passing else.
         if self.strat == 0:
             if pass_mult == 2 and other_poison_left > 0:
                 return 'pass'
             else:
-                return 'shoot'
-        elif self.strat == 1:
+                if np.random.rand() < 0.2:
+                    return 'pass'
+                else:
+                    return 'shoot'
 
+        # Strat 1: pass if probability of opponent poison bullet < certain probaility threshold
+        elif self.strat == 1:
             if pass_mult == 2:
                 return 'pass'
             elif self.mag[bullet] == 1:
@@ -223,16 +234,18 @@ class player:
                 else:
                     return 'shoot'
 
+
+# Plot outcomes
 x = np.arange(100) / 100
 
 y = []
-
+n = 30000
 for i in range(len(x)):
     y_ = []
-    for j in range(100000):
+    for j in range(n):
         bot = rrbot()
         p1 = player(pass_prob=x[i], strat=1)
-        p2 = player()
+        p2 = player(pass_prob=0.2, strat=1)
         p1_score, p2_score = bot.game_start(p1, p2)
         if p1_score > p2_score:
             y_.append(1)
@@ -240,7 +253,7 @@ for i in range(len(x)):
             y_.append(-1)
         else:
             y_.append(0)
-    y.append(np.sum(y_))
+    y.append(np.sum(y_) / n)
 
 y = np.array(y)
 
